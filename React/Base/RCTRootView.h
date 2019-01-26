d28fcbae4c5a89578b382e8d28bda2a060a32f8f
aleclarson <alec.stanford.larson@gmail.com>
2019-01-26 14:20:39 -0500

    fix: make RCTRootView inherit from NSView
    Inheriting from NSVisualEffectView means we cannot have a transparent root view.

--- React/Base/RCTRootView.h
+++ React/Base/RCTRootView.h
@@ -39,7 +39,7 @@ extern NSString *const RCTContentDidAppearNotification;
  * like any ordinary UIView. You can have multiple RCTRootViews on screen at
  * once, all controlled by the same JavaScript application.
  */
-@interface RCTRootView : NSVisualEffectView
+@interface RCTRootView : NSView
 
 /**
  * - Designated initializer -
