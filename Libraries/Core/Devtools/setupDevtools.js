3e2c3994862be5946fb936f5aa47cf282f804b5b
Alec Larson <alec.stanford.larson@gmail.com>
2019-10-03 15:47:39 -0400

    chore: upgrade devtools

--- Libraries/Core/Devtools/setupDevtools.js
+++ Libraries/Core/Devtools/setupDevtools.js
@@ -11,54 +11,70 @@
  */
 'use strict';
 
-type DevToolsPluginConnection = {
-  isAppActive: () => boolean,
-  host: string,
-  port: number,
-};
+const Platform = require('Platform');
 
-type DevToolsPlugin = {
-  connectToDevTools: (connection: DevToolsPluginConnection) => void,
-};
-
-let register = function () {
-  // noop
-};
-
+/**
+ * Sets up developer tools for React Native.
+ * You can use this module directly, or just require InitializeCore.
+ */
 if (__DEV__) {
-  const AppState = require('AppState');
-  const WebSocket = require('WebSocket');
-  /* $FlowFixMe(>=0.54.0 site=react_native_oss) This comment suppresses an
-   * error found when Flow v0.54 was deployed. To see the error delete this
-   * comment and run Flow. */
-  const reactDevTools = require('react-devtools-core');
-  const getDevServer = require('getDevServer');
+  // TODO (T45803484) Enable devtools for bridgeless RN
+  if (!global.RN$Bridgeless) {
+    if (!global.__RCTProfileIsProfiling) {
+      // not when debugging in chrome
+      // TODO(t12832058) This check is broken
+      if (!window.document) {
+        const AppState = require('AppState');
+        // $FlowFixMe Module is untyped
+        const reactDevTools = require('react-devtools-core');
+        const getDevServer = require('getDevServer');
 
-  // Initialize dev tools only if the native module for WebSocket is available
-  if (WebSocket.isAvailable) {
-    // Don't steal the DevTools from currently active app.
-    // Note: if you add any AppState subscriptions to this file,
-    // you will also need to guard against `AppState.isAvailable`,
-    // or the code will throw for bundles that don't have it.
-    const isAppActive = () => AppState.currentState !== 'background';
+        // Don't steal the DevTools from currently active app.
+        // Note: if you add any AppState subscriptions to this file,
+        // you will also need to guard against `AppState.isAvailable`,
+        // or the code will throw for bundles that don't have it.
+        const isAppActive = () => AppState.currentState !== 'background';
 
-    // Get hostname from development server (packager)
-    const devServer = getDevServer();
-    const host = devServer.bundleLoadedFromServer
-      ? devServer.url.replace(/https?:\/\//, '').split(':')[0]
-      : 'localhost';
+        // Get hostname from development server (packager)
+        const devServer = getDevServer();
+        const host = devServer.bundleLoadedFromServer
+          ? devServer.url.replace(/https?:\/\//, '').split(':')[0]
+          : 'localhost';
 
-    reactDevTools.connectToDevTools({
-      isAppActive,
-      host,
-      // Read the optional global variable for backward compatibility.
-      // It was added in https://github.com/facebook/react-native/commit/bf2b435322e89d0aeee8792b1c6e04656c2719a0.
-      port: window.__REACT_DEVTOOLS_PORT__,
-      resolveRNStyle: require('flattenStyle'),
-    });
+        reactDevTools.connectToDevTools({
+          isAppActive,
+          host,
+          // Read the optional global variable for backward compatibility.
+          // It was added in https://github.com/facebook/react-native/commit/bf2b435322e89d0aeee8792b1c6e04656c2719a0.
+          port: window.__REACT_DEVTOOLS_PORT__,
+          resolveRNStyle: require('flattenStyle'),
+        });
+      }
+
+      // Set up inspector
+      const JSInspector = require('JSInspector');
+      JSInspector.registerAgent(require('NetworkAgent'));
+    }
+
+    if (!Platform.isTesting) {
+      const HMRClient = require('HMRClient');
+      [
+        'trace',
+        'info',
+        'warn',
+        'log',
+        'group',
+        'groupCollapsed',
+        'groupEnd',
+        'debug',
+      ].forEach(level => {
+        const originalFunction = console[level];
+        // $FlowFixMe Overwrite console methods
+        console[level] = function(...args) {
+          HMRClient.log(level, args);
+          originalFunction.apply(console, args);
+        };
+      });
+    }
   }
 }
-
-module.exports = {
-  register,
-};
